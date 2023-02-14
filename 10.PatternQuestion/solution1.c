432ff102b442f4218d1de91697f778b623235a2f
Shivansh <shivanshkumar760@gmail.com>
2023-02-15 00:37:38 +0530

    third commit

---
+++ 10.PatternQuestion/solution1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    n=5;
+    for(int i=1;i<=n;i++)
+    {
+       
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+       
+    }
+}
---
+++ 10.PatternQuestion/solution2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+}

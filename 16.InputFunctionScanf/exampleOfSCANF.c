3a0bee8437ab03ace449c7522fab9231ff0a688b
Shivansh <shivanshkumar760@gmail.com>
2023-03-03 23:46:12 +0530

    Ninth commit

---
+++ 16.InputFunctionScanf/exampleOfSCANF.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;//integer variable 
+    printf("Enter a nummber for variable a:");
+    scanf("%d", &a);
+    printf("Value of a is :%d\n", a);
+    return 0;
+}
